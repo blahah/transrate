9f8dd161393d0469094820ecc121c4b6d76109bf
Richard Smith <rds45@cam.ac.uk>
2013-09-26 13:55:26 +0100

    single assembly score by euclidean distance (wip) #1

---
+++ lib/transrate/dimension_reduce.rb
@@ -0,0 +1,18 @@
+module Transrate
+
+  class DimensionReduce
+
+    def self.dimension_reduce(metrics)
+      total = 0
+      metrics.each do |metric|
+        o = metric.origin
+        w = metric.weighting
+        a = metric.score
+        total += w * ((o - a) ** 2)
+      end
+      Math.sqrt(total) / bmetrics.length
+    end
+      
+  end # DimensionReduce
+
+end # Transrate
--- lib/transrate/transrater.rb
+++ lib/transrate/transrater.rb
@@ -7,7 +7,7 @@ module Transrate
     attr_reader :comparative_metrics
 
     def initialize assembly, reference, left, right, insertsize=nil, insertsd=nil
-      @assembly = Assembly.new assembly
+      @assembly = assembly.is_a?(Assembly) ? assembly : Assembly.new(assembly)
       @reference = reference.is_a?(Assembly) ? reference : Assembly.new(reference)
       @read_metrics = ReadMetrics.new @assembly
       @comparative_metrics = ComparativeMetrics.new(@assembly, @reference)
@@ -20,6 +20,14 @@ module Transrate
       @comparative_metrics.run
     end
 
+    def assembly_score
+      pg = @read_metrics.pc_good_mapping
+      pg = Metric.new('pg', pg, 0)
+      rh = @comparative_metrics.reciprocal_hits
+      rh = Metric.new('rh', rh, 0)
+      DimensionReduce.dimension_reduce([pg, rh])
+    end
+    
   end # Transrater
 
 end # Transrate
