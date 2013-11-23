d473b435a7623e2e1a191ad84ce0fce840244ae3
Richard Smith <rds45@cam.ac.uk>
2013-11-23 23:05:19 +0000

    setup for testing

---
+++ test/helper.rb
@@ -0,0 +1,16 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
+require 'test/unit'
+begin; require 'turn/autorun'; rescue LoadError; end
+require 'shoulda-context'
+require 'transrate'
+
+Turn.config.format = :pretty
+Turn.config.trace = 5
