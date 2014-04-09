f67c4a13a68d9d7e9ab5c688e7cab1dc464a26d7
Richard Smith <rds45@cam.ac.uk>
2014-04-09 11:28:05 +0100

    add large test files to gitignore
    Former-commit-id: c916d7ac2ed7fa5385896f6d1fb731e249c5605a

--- test/data/testrun.sh
+++
@@ -1,9 +0,0 @@
-mkdir tmp
-cd tmp
-bundle exec ../../../bin/transrate \
-  --assembly ../assembly.fa \
-  --reference ../reference.fa \
-  --left ../72pc.l.fq \
-  --right ../72pc.r.fq \
-  --threads 4
-rm -rf tmp
