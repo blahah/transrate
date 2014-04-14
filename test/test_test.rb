7effa949858946a6af0d9f35cbfd071b3073e398
Chris Boursnell <cboursnell@gmail.com>
2014-04-14 13:09:44 +0100

    string and line length cleanup

--- test/test_test.rb
+++ test/test_test.rb
@@ -7,15 +7,15 @@ class TestTransrate < Test::Unit::TestCase
   context "transrate" do
 
     setup do
-      @a = Transrate::Assembly.new("test/assembly.fasta")
+      @a = Transrate::Assembly.new('test/assembly.fasta')
     end
 
-    should "run metrics on assembly" do
+    should 'run metrics on assembly' do
       ans = @a.run(2) # using 2 threads
       assert_equal ans, true, "should run but returned #{ans}"
     end
 
-    should "find the mean length" do
+    should 'find the mean length' do
       ans = @a.run(2)
       mean = @a.mean_len
       assert_equal mean, 1508.25
