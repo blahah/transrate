0df41d954d52aba319f8cfd4d527b24371652d3f
Richard Smith <rds45@cam.ac.uk>
2014-04-14 17:58:53 +0100

    remove logging for now

--- lib/transrate.rb
+++ lib/transrate.rb
@@ -10,14 +10,7 @@ require 'transrate/comparative_metrics'
 require 'transrate/metric'
 require 'transrate/dimension_reduce'
 require 'transrate/express'
-require 'transrate/log'
 
 module Transrate
-
-  # one logger instance is stored as a module ivar
-  # to avoid creating lots of loggers
-  class << self
-    attr_accessor :log
-  end # log accessor
 
 end # Transrate
