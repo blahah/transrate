da8a110a3ea79c328b6ee0e2ccc4e63742888ea1
Chris Boursnell <cboursnell@gmail.com>
2014-04-16 11:02:49 +0100

    Took the C out of a string and put it in a SRC block
    Former-commit-id: ff88253fa99c34cae7c3e3630152fd87a2b93901

--- lib/transrate/assembly.rb
+++ lib/transrate/assembly.rb
@@ -227,51 +227,52 @@ module Transrate
      
     inline do |builder|
 
-    builder.c "
-      static
-      void
-      longest_orf(VALUE _s) {
-        int i,sl,longest=0;
-        int len[6];
-        char * c_str;
+      builder.c <<SRC
+        static
+        void
+        longest_orf(VALUE _s) {
+          int i,sl,longest=0;
+          int len[6];
+          char * c_str;
 
-        sl = RSTRING_LEN(_s);
-        c_str = StringValueCStr(_s);
-        for (i=0;i<6;i++) {
-          len[i]=0;
-        }
-        for (i=0;i<sl-2;i++) {
-          if (c_str[i]=='T' &&
-            ((c_str[i+1]=='A' && c_str[i+2]=='G') ||
-            (c_str[i+1]=='A' && c_str[i+2]=='A') ||
-            (c_str[i+1]=='G' && c_str[i+2]=='A'))) { 
-            if (len[i%3] > longest) {
-              longest = len[i%3];
-            }
-            len[i%3]=0;
-          } else {
-            len[i%3]++;
+          sl = RSTRING_LEN(_s);
+          c_str = StringValueCStr(_s);
+          for (i=0;i<6;i++) {
+            len[i]=0;
           }
-          if (c_str[i+2]=='A' &&
-            ((c_str[i]=='C' && c_str[i+1]=='T') ||
-            (c_str[i]=='T' && c_str[i+1]=='T') ||
-            (c_str[i]=='T' && c_str[i+1]=='C'))) { 
-            if (len[3+i%3] > longest) {
-              longest = len[3+i%3];
+          for (i=0;i<sl-2;i++) {
+            if (c_str[i]=='T' &&
+              ((c_str[i+1]=='A' && c_str[i+2]=='G') ||
+              (c_str[i+1]=='A' && c_str[i+2]=='A') ||
+              (c_str[i+1]=='G' && c_str[i+2]=='A'))) { 
+              if (len[i%3] > longest) {
+                longest = len[i%3];
+              }
+              len[i%3]=0;
+            } else {
+              len[i%3]++;
             }
-            len[3+i%3]=0;
-          } else {
-            len[3+i%3]++;
+            if (c_str[i+2]=='A' &&
+              ((c_str[i]=='C' && c_str[i+1]=='T') ||
+              (c_str[i]=='T' && c_str[i+1]=='T') ||
+              (c_str[i]=='T' && c_str[i+1]=='C'))) { 
+              if (len[3+i%3] > longest) {
+                longest = len[3+i%3];
+              }
+              len[3+i%3]=0;
+            } else {
+              len[3+i%3]++;
+            }
           }
+          if (len[i%3] > longest) {
+            longest = len[i%3];
+          }
+          if (len[3+i%3] > longest) {
+            longest = len[3+i%3];
+          }
+          return INT2NUM(longest);
         }
-        if (len[i%3] > longest) {
-          longest = len[i%3];
-        }
-        if (len[3+i%3] > longest) {
-          longest = len[3+i%3];
-        }
-        return INT2NUM(longest);
-      }"
+SRC
     end
 
     # finds longest orf in a sequence
