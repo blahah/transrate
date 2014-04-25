f76ab5e0b886cebc1ad76be996ef372b00eb27e5
Chris Boursnell <cboursnell@gmail.com>
2014-04-25 09:59:43 +0100

    removed redundant tests
    Former-commit-id: cca758e2ed12c2a8287401eecb2eafc95948a9a8

--- test/test_test.rb
+++ test/test_test.rb
@@ -8,27 +8,11 @@ class TestTransrate < Test::Unit::TestCase
 
     setup do
       @a = Transrate::Assembly.new("test/assembly.fasta")
-      @seq1 = "ATGCCCGGGTAG"
     end
 
     should "run metrics on assembly" do
       ans = @a.run(2) # using 2 threads
       assert_equal ans, true, "should run but returned #{ans}"
-    end
-
-    should "find longest orf" do
-      len = @a.orf_length("ATGCCCGGGTAG")
-      assert_equal len, 3, "expected 4 but got #{len}"
-    end
-
-    should "find longest orf in file" do
-      orfs = []
-      @a.assembly.each do |entry|
-        l = @a.orf_length entry.seq
-        orfs << l
-      end
-      assert_equal orfs.length, 4
-      assert_equal orfs, [333, 370, 131, 84]
     end
 
     should "find the mean length" do
