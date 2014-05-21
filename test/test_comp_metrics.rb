4ec69e05ae45d0bfb0620516610ad642de4346f5
Chris Boursnell <cboursnell@gmail.com>
2014-05-21 12:43:01 +0100

    added fasta files for use in tests
    Former-commit-id: 01665161b9e0f9f16e55bec8ca0a8c889c841e31

--- test/test_comp_metrics.rb
+++ test/test_comp_metrics.rb
@@ -7,8 +7,8 @@ class TestCompMetrics < Test::Unit::TestCase
   context "transrate" do
 
     setup do
-      query = "test_data/cd.v3.soap.fa"
-      target = "test_data/Os.protein.fa"
+      query = "test/clg.assembly.fa"
+      target = "test/Os.protein.fa"
       assembly = Transrate::Assembly.new(query)
       reference = Transrate::Assembly.new(target)
       threads = 8
