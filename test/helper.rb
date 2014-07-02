d61cfe9518c3a309499697d47e8169d7c3c98a11
Chris Boursnell <cboursnell@gmail.com>
2014-07-02 15:29:01 +0100

    add crbblast mocking class

--- test/helper.rb
+++ test/helper.rb
@@ -7,11 +7,39 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 ]
 SimpleCov.start
 
-# require 'test/unit'
-# require 'minitest/autorun'
+require 'minitest/autorun'
 begin; require 'turn/autorun'; rescue LoadError; end
 require 'shoulda/context'
 require 'transrate'
 
 Turn.config.format = :pretty
 Turn.config.trace = 5
+
+# fake CRBBlast class
+class CRBHelper
+
+  attr_accessor :target_is_prot, :hash
+  def initialize t
+    @target_is_prot = t
+  end
+
+  def reciprocals
+    return @hash
+  end
+end
+
+# rake Hit class
+class HitHelper
+  attr_accessor :query, :target, :qstart, :qend, :tstart, :tend, :qlen, :tlen
+  def initialize query, target, qstart, qend, tstart, tend, qlen, tlen
+    @query = query
+    @target = target
+    @qstart = qstart
+    @tstart = tstart
+    @tend = tend
+    @qend = qend
+    @qlen = qlen
+    @tlen = tlen
+  end
+
+end
