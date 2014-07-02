41569ef2a92a23b5560d1d599d441b6a2a8a3ab4
Chris Boursnell <cboursnell@gmail.com>
2014-07-02 16:25:59 +0100

    added large tests with clear descriptions of coverage calculation

--- test/test_comp_metrics.rb
+++ test/test_comp_metrics.rb
@@ -30,23 +30,60 @@ class TestCompMetrics < Test::Unit::TestCase
       (1..11).each do |i|
         hash["q#{i}"] = []
       end
-      hash["q1"] << HitHelper.new("q1", "t1", 101, 300, 1, 400, 400, 400)
-      hash["q2"] << HitHelper.new("q2", "t1", 201, 400, 1, 400, 400, 400)
-      hash["q3"] << HitHelper.new("q3", "t2", 110, 290, 1, 400, 400, 400)
-      hash["q4"] << HitHelper.new("q4", "t2", 101, 300, 1, 400, 400, 400)
-      hash["q5"] << HitHelper.new("q5", "t3", 200, 400, 1, 400, 400, 400)
-      hash["q6"] << HitHelper.new("q6", "t3", 101, 300, 1, 400, 400, 400)
-      hash["q7"] << HitHelper.new("q7", "t4", 101, 200, 1, 400, 400, 400)
-      hash["q8"] << HitHelper.new("q8", "t4", 301, 400, 1, 400, 400, 400)
-      hash["q9"] << HitHelper.new("q9", "t5", 101, 200, 1, 400, 400, 400)
-      hash["q10"] << HitHelper.new("q10", "t5", 301, 400, 1, 400, 400, 400)
-      hash["q11"] << HitHelper.new("q11", "t5", 150, 350, 1, 400, 400, 400)
+      # Q1   |------------|
+      # Q2                    |--------------|
+      # T1 |------------------------------------|  # coverage = 200/500
+      hash["q1"] << HitHelper.new("q1", "t1", 1, 100, 101, 200, 100, 500)
+      hash["q2"] << HitHelper.new("q2", "t1", 1, 100, 301, 400, 100, 500)
+
+      # Q3                    |------------|
+      # Q4   |--------------|
+      # T2 |------------------------------------| # coverage = 200/500
+      hash["q3"] << HitHelper.new("q3", "t2", 1, 100, 301, 400, 100, 500)
+      hash["q4"] << HitHelper.new("q4", "t2", 1, 100, 101, 200, 100, 500)
+
+      # Q5                |------------|
+      # Q6   |-------------------|
+      # T3 |------------------------------------| # coverage = 300/500
+      hash["q5"] << HitHelper.new("q5", "t3", 1, 200, 201, 400, 200, 500)
+      hash["q6"] << HitHelper.new("q6", "t3", 1, 200, 101, 300, 200, 500)
+
+      # Q7             |------------|
+      # Q8      |------------------------|
+      # T3 |------------------------------------| # coverage = 300/500
+      hash["q7"] << HitHelper.new("q7", "t4", 1, 100, 201, 300, 100, 500)
+      hash["q8"] << HitHelper.new("q8", "t4", 1, 300, 101, 400, 300, 500)
+
+      # Q9     |--------|
+      # Q10                        |--------|
+      # Q11        |--------------------|
+      # T5 |------------------------------------| # coverage = 600/1000
+      hash["q9"] << HitHelper.new("q9",   "t5", 1, 200, 201, 400, 200, 1000)
+      hash["q10"] << HitHelper.new("q10", "t5", 1, 200, 601, 800, 200, 1000)
+      hash["q11"] << HitHelper.new("q11", "t5", 1, 400, 301, 700, 400, 1000)
+
       crb.hash = hash
       ohr = @comp.ortholog_hit_ratio crb
-      assert_equal 0.65, ohr
+      assert_equal 16.0/30.0, ohr
     end
 
     should "calculate potential chimera count" do
+      crb = CRBHelper.new(false)
+
+      hash = Hash.new
+      (1..2).each do |i|
+        hash["q#{i}"] = []
+      end
+
+      # T1   |---------|
+      # T2                 |---------|
+      # Q1 |----------------------------| # chimera = true
+      hash["q1"] << HitHelper.new("q1", "t1", 101, 200, 1, 100, 500, 100)
+      hash["q1"] << HitHelper.new("q1", "t2", 301, 400, 1, 100, 400, 100)
+
+      crb.hash = hash
+      chi = @comp.ortholog_hit_ratio crb
+      assert_equal 1, chi
     end
 
     should "calculate number of contigs with crbblast hit" do
