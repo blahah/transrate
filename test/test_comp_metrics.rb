543c9ee320ff347303e58c4c39500de2eb401182
Chris Boursnell <cboursnell@gmail.com>
2014-07-02 15:32:34 +0100

    removed unnecessary teardown method

--- test/test_comp_metrics.rb
+++ test/test_comp_metrics.rb
@@ -17,20 +17,6 @@ class TestCompMetrics < Test::Unit::TestCase
       @comp = Transrate::ComparativeMetrics.new(assembly, reference, threads)
     end
 
-    teardown do
-      File.delete("test/assembly.nsq") if File.exist?("test/assembly.nsq")
-      File.delete("test/assembly.nin") if File.exist?("test/assembly.nin")
-      File.delete("test/assembly.nhr") if File.exist?("test/assembly.nhr")
-      File.delete("test/Os.protein.psq") if File.exist?("test/Os.protein.psq")
-      File.delete("test/Os.protein.pin") if File.exist?("test/Os.protein.pin")
-      File.delete("test/Os.protein.phr") if File.exist?("test/Os.protein.phr")
-      if File.exist?("test/assembly_into_Os.protein.1.blast")
-        File.delete("test/assembly_into_Os.protein.1.blast")
-      end
-      if File.exist?("test/Os.protein_into_assembly.2.blast")
-        File.delete("test/Os.protein_into_assembly.2.blast")
-      end
-    end
 
     should "run metrics on assembly" do
       @comp.run
