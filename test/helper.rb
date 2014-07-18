227fe9df58f74fe3cdcf5346d9d831f25a2f3394
Chris Boursnell <cboursnell@gmail.com>
2014-07-18 15:24:57 +0100

    removed mocking classes; not needed now

--- test/helper.rb
+++ test/helper.rb
@@ -14,34 +14,3 @@ require 'transrate'
 
 Turn.config.format = :pretty
 Turn.config.trace = 5
-
-# fake CRBBlast class
-class CRBHelper
-
-  attr_accessor :target_is_prot, :hash
-  def initialize t
-    @target_is_prot = t
-  end
-
-  def reciprocals
-    return @hash
-  end
-
-end
-
-# rake Hit class
-class HitHelper
-
-  attr_accessor :query, :target, :qstart, :qend, :tstart, :tend, :qlen, :tlen
-  def initialize query, target, qstart, qend, tstart, tend, qlen, tlen
-    @query = query
-    @target = target
-    @qstart = qstart
-    @tstart = tstart
-    @tend = tend
-    @qend = qend
-    @qlen = qlen
-    @tlen = tlen
-  end
-
-end
