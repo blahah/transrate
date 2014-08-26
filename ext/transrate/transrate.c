f0c2fbb5edbb83e1d24fadf599cb9d69fad44b4c
Chris Boursnell <cboursnell@gmail.com>
2014-08-26 17:20:37 +0100

    changed how orfs are calculated

--- ext/transrate/transrate.c
+++ ext/transrate/transrate.c
@@ -558,47 +558,77 @@ VALUE method_kmer_count(VALUE self, VALUE _k, VALUE _s) {
 // takes in a string and calculates the longest open reading frame
 // in any of the 6 frames
 // an open reading frame is defined as the number of bases between
-// either the start of the sequence or a stop codon and either the
+// either the start of the sequence or a start codon and either the
 // end of the sequence or a stop codon
-VALUE method_longest_orf(VALUE self, VALUE _s) {
-  int i,sl,longest=0;
-  int len[6];
-  char * c_str;
 
-  sl = RSTRING_LEN(_s);
-  c_str = StringValueCStr(_s);
-  for (i=0;i<6;i++) {
+VALUE method_longest_orf(VALUE self, VALUE _str) {
+  int i,sl,longest=0;
+  int len[3];
+  char * str;
+  sl = RSTRING_LEN(_str);
+  str = StringValueCStr(_str);
+  for (i=0;i<3;i++) {
     len[i]=0;
   }
-  for (i=0;i<sl-2;i++) {
-    if (c_str[i]=='T' &&
-      ((c_str[i+1]=='A' && c_str[i+2]=='G') ||
-      (c_str[i+1]=='A' && c_str[i+2]=='A') ||
-      (c_str[i+1]=='G' && c_str[i+2]=='A'))) {
-      if (len[i%3] > longest) {
-        longest = len[i%3];
+  for(i=0;i<sl-2;i++) {
+    if (str[i]=='A' && str[i+1]=='T' && str[i+2]=='G') { //Methionine
+      if (len[i%3]>=0) {
+        len[i%3]++;
+      } else {
+        len[i%3]=1;
       }
-      len[i%3]=0;
     } else {
-      len[i%3]++;
-    }
-    if (c_str[i+2]=='A' &&
-      ((c_str[i]=='C' && c_str[i+1]=='T') ||
-      (c_str[i]=='T' && c_str[i+1]=='T') ||
-      (c_str[i]=='T' && c_str[i+1]=='C'))) {
-      if (len[3+i%3] > longest) {
-        longest = len[3+i%3];
+      if (str[i]=='T' &&
+        ((str[i+1]=='A' && str[i+2]=='G') ||   //amber
+        (str[i+1]=='A' && str[i+2]=='A') ||    //ochre   stops
+        (str[i+1]=='G' && str[i+2]=='A'))) {   //umber
+        if (len[i%3]>longest) {
+          longest = len[i%3];
+        }
+        len[i%3]=-1;
+      } else { // any other codon
+        if (len[i%3]>=0) {
+          len[i%3]++;
+        }
       }
-      len[3+i%3]=0;
-    } else {
-      len[3+i%3]++;
     }
   }
-  if (len[i%3] > longest) {
-    longest = len[i%3];
+  for(i=0;i<3;i++) {
+    if (len[i%3] > longest) {
+      longest = len[i%3];
+    }
   }
-  if (len[3+i%3] > longest) {
-    longest = len[3+i%3];
+  for (i=0;i<3;i++) {
+    len[i]=0;
+  }
+  for(i=sl-1;i>=2;i--) {
+    if (str[i]=='T' && str[i-1]=='A' && str[i-2]=='C') { //Methionine
+      if (len[i%3]>=0) {
+        len[i%3]++;
+      } else {
+        len[i%3]=1;
+      }
+    } else {
+      if (str[i]=='A' &&
+        ((str[i-1]=='T' && str[i-2]=='C') ||   //amber
+        (str[i-1]=='T' && str[i-2]=='T') ||    //ochre   stops
+        (str[i-1]=='C' && str[i-2]=='T'))) {   //umber
+        if (len[i%3]>longest) {
+          longest = len[i%3];
+        }
+        len[i%3]=-1;
+      } else { // any other codon
+        if (len[i%3]>=0) {
+          len[i%3]++;
+        }
+      }
+    }
+  }
+  for(i=0;i<3;i++) {
+    if (len[i%3] > longest) {
+      longest = len[i%3];
+    }
   }
   return INT2NUM(longest);
 }
+
