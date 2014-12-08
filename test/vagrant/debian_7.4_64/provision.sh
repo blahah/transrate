b21f5e580a6edb7744f5b07f6c71fe757c8b215f
Richard Smith-Unna <richardsmith404@gmail.com>
2014-12-08 13:32:09 +0000

    add debian, update provisioning

--- test/vagrant/debian_7.4_64/provision.sh
+++ test/vagrant/debian_7.4_64/provision.sh
@@ -1,6 +1,6 @@
 # Setup
 sudo apt-get update -y
-sudo apt-get install -y curl build-essential git
+sudo apt-get install -y curl build-essential git zlib1g zlib1g-dev cmake
 
 # Install a multi-user RVM install with the latest ruby
 sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
@@ -14,6 +14,10 @@ sudo usermod -a -G rvm vagrant
 git clone https://github.com/Blahah/transrate.git
 cd transrate
 sudo chmod -R 777 .
+
+# Install transrate-tools
+cd ..
+git clone https://github.com/Blahah/transrate-tools.git
 
 # Install dependencies
 gem install bundler rake
